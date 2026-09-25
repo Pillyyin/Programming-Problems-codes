d581c2e5d4fae931e501e62160771232eb1ee56c
Pillyyin <pillyyin@gmail.com>
2026-09-25 17:20:39 +0800

    Daily C++ sync - 2026-09-24

---
+++ LeetCode/Easy/0867-transpose-matrix.cpp
@@ -0,0 +1,15 @@
+//  Transpose a matrix by using one dimension array
+//  Runtime 
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        struct Element{
+            int row ;
+            int col ;
+            int value ;
+        };
+
+        int arr[matrix.size()*matrix[0].size()] ;
+
+    }
+};
---
+++ LeetCode/Easy/Finish/0766-toeplitz-matrix.cpp
@@ -0,0 +1,19 @@
+//  Runtime 0ms(100%), Memory 21.14MB(25.96%) 
+class Solution {
+public:
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+        int row = matrix.size() ;
+        int col = matrix[0].size() ;
+
+        for(int i=1;i<row;i++){
+            for(int j=1;j<col;j++){
+                if(matrix[i][j] != matrix[i-1][j-1]){
+                    return false ;
+                }
+            }
+        }
+
+        return true ;
+    }
+};
+
